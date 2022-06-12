1b2bba15e71cb918432fed776b5fbb12d81c746c
Hamish MacDonald <elusivenode@gmail.com>
2022-06-12 22:35:46 +1000

    Draw another RHB.  Using frame, hashmap and sprite sheet

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
+use serde::Deserialize;
+use std::{collections::HashMap, rc::Rc, sync::Mutex};
 use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures;
 use web_sys;
-use std::{collections::HashMap, rc::Rc, sync::Mutex};
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {frame: Rect,
+}
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -26,8 +44,42 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
     
+    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (0, 255, 0), 4);
+
     wasm_bindgen_futures::spawn_local(async move {
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+            
         let image = web_sys::HtmlImageElement::new().unwrap();
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+    
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok()
+                .and_then(|mut opt| opt.take()) {
+                    success_tx.send(Ok(()));
+                }
+        });
+    
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok()
+                .and_then(|mut opt| opt.take()) {
+                    error_tx.send(Err(err));
+                }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("Idle (1).png");
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
         let success_tx = Rc::new(Mutex::new(Some(success_tx)));
         let error_tx = Rc::clone(&success_tx);
@@ -46,14 +98,27 @@ pub fn main_js() -> Result<(), JsValue> {
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
         image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
+        image.set_src("rhb.png");
         success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
 
-        sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (0, 255, 0), 4);
+        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &image,
+            sprite.frame.x.into(), sprite.frame.y.into(), sprite.frame.w.into(), sprite.frame.h.into(),
+            300.0, 300.0, sprite.frame.w.into(), sprite.frame.h.into(),
+        );
+        
     });
 
     Ok(())
+}
+
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(
+        window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
 
 fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8, u8, u8), depth: u8) {
